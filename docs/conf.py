a7cb089783a06ba16678072addc1656d4f29958f
Alleayy <Daipooran@outlook.com>
2025-06-21 15:17:29 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,14 +9,6 @@ exclude_patterns = []
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-{% extends "!layout.html" %}
-
-{% block meta %}
-  {{ super() }}
-  <meta name="google-site-verification" content="vWuKdfn7mBaw-KlLRh8j_GtlIs4FQ-h0VcG-WO8D8ns" />
-  <meta name="msvalidate.01" content="YOUR_BING_VERIFICATION_CODE">
-{% endblock %}
-
 
 html_js_files = [
     'chat.js',

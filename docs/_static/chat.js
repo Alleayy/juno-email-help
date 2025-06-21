1de9cc6c524118b420f52c5fe8dbe1cb929b5028
Alleayy <Daipooran@outlook.com>
2025-06-21 15:40:16 +0530

    Update chat.js

--- docs/_static/chat.js
+++ docs/_static/chat.js
@@ -4,3 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
     ze.src = 'https://static.zdassets.com/ekr/snippet.js?key=7668c976-0fb7-47ee-8740-3e9115bdb10f';
     document.body.appendChild(ze);
 });
+document.addEventListener("DOMContentLoaded", function () {
+  var meta = document.createElement("meta");
+  meta.name = "google-site-verification";
+  meta.content = "vWuKdfn7mBaw-KlLRh8j_GtlIs4FQ-h0VcG-WO8D8ns";
+  document.getElementsByTagName("head")[0].appendChild(meta);
+});
